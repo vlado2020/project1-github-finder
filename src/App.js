1b4a0a62533d93de2e89b622bcc1c1d79d5a2b92
vlado2020 <vladimir.puljko1@gmail.com>
2021-02-17 22:03:06 +0100

    mojombo, početak rada na github-finderu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import "./App.css";
+import Navbar from './components/layout/Navbar'
+import UserItem from './components/users/UserItem'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  constructor(){
+    super();
+    console.log(123);
+  }
+
+  render() {
+    return (
+
+    <div className='App'> 
+        <Navbar title="Github tragač" icon = 'fab fa-github' />
+        <UserItem/>
     </div>
-  );
-}
 
+    
+
+    );
+ }
+}
 export default App;
+
+
+    // const name= 'Vladiša'
+    // const foo = ()=> "bar"
+    // return (
+      // <Fragment>
+      //   <h3>Tu je naslov</h3>
+      //   <h2>Doviđenja {name.toUpperCase()}</h2>
+      //   <h2>Adieu {foo()}</h2>
+      //   <h3>{this.met}</h3>
+
+
+      // </Fragment>
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import UserItem from "./UserItem";
+
+class Users extends Component {
+  state = {
+    id: "id",
+    login: "mojombo",
+    avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+    html_url: "https://github.com/mojombo",
+  };
+
+  render() {
+    const { login, avatar_url, html_url } = this.state;
+    return (
+      <div className="card text-center">
+        <img
+          src={avatar_url}
+          alt=""
+          className="round-img"
+          style={{ width: "400px" }}
+        />
+        <h3>{login}</h3>
+
+        <div>
+          <a href={html_url} className="btn btn-dark btn-sm my-1">
+            Još
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+const userStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gridGap: "1rem",
+};
+
+export default Users;
